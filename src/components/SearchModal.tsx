10cb739dc5273e0e14e5f279981c8f15b74036ca
darkhorse0725 <darktop0112@gmail.com>
2023-03-07 14:06:45 +0900

    added search bar

---
+++ src/components/SearchModal.tsx
@@ -0,0 +1,163 @@
+import axios from 'axios';
+import { ENV } from 'lib/env';
+import React , { useState, useRef, useCallback, useEffect} from 'react'
+import { useInfiniteQuery, useQueryClient } from 'react-query'
+import { IPost } from 'types/interfaces';
+
+import Input from './Input'
+import MinifiedPost from './pages/home/MinifiedPost';
+import { BiLoaderAlt } from 'react-icons/bi';
+
+interface SearchModalProps {
+}
+
+const SearchModal = ({} : SearchModalProps) => {
+    const limit = 5;
+
+    const [visible, setVisible] = useState(false)
+    const [searchKey, setSearchKey]  = useState('')
+    const [selectedPost, setSelectedPost] = useState<IPost | null>(null)
+    const observerElem = useRef(null)
+    const queryClient = useQueryClient()
+
+    const handleSearchKey = (value: string) => { 
+        // eslint-disable-next-line no-console
+        queryClient.invalidateQueries('getSearchedPosts')
+        setSearchKey(value)
+     }
+    
+    const toggleEvent = () => {
+        setVisible(true)
+    }
+
+    const blurEvent = () => {
+        setVisible(false)
+    }
+
+    const toggleEditPostModal = () => {}
+
+    const handleSelectedPost = (post?: IPost) => {
+        if (!post) return setSelectedPost(null)
+        setSelectedPost(post)
+    }
+
+    const fetchExplorePosts = async (page: Number) => {
+        const response = await axios.get(
+          `${ENV.API_URL}/getSearchPosts?per_page=${limit}&page=${page}&search=${searchKey}`
+        )
+        return {
+          result: response.data.data
+        }
+    }
+    
+    const {
+        data,
+        isSuccess,
+        hasNextPage,
+        fetchNextPage,
+        isFetchingNextPage
+    } = useInfiniteQuery(
+        'getSearchedPosts',
+        ({ pageParam = 1 }) => fetchExplorePosts(pageParam),
+        {
+            getNextPageParam: (lastPage, allPages) => {
+                const nextPage: number = allPages.length + 1
+                return lastPage.result.length === limit ? nextPage : undefined
+            }
+        }
+    )
+    
+    const handleObserver = useCallback(
+        (entries: IntersectionObserverEntry[]) => {
+            // eslint-disable-next-line no-console
+            console.log('here')
+            const [target] = entries
+            if (target.isIntersecting && hasNextPage) {
+                fetchNextPage()
+            }
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [fetchNextPage, hasNextPage]
+    )
+    
+    useEffect(() => {
+        if (!observerElem.current) return
+        const element: HTMLDivElement = observerElem.current
+        const option = { threshold: 0 }
+    
+        const observer = new IntersectionObserver(handleObserver, option)
+        observer.observe(element)
+        return () => observer.unobserve(element)
+    }, [fetchNextPage, hasNextPage, handleObserver])
+
+    return (
+        <div className='relative' onMouseOver={() => setVisible(true)} onMouseOut={() => setVisible(false)}>
+           <Input
+                onKeyUp={fetchNextPage}
+                onChange={handleSearchKey}
+                value={searchKey}
+                placeholder='Enter Search Key'
+                type='text'
+                inputClassName='rounded-none placeholder:text-[#666]'
+                inputProps={{
+                  maxLength: 120
+                }}
+                onFocus={() => toggleEvent()}
+            />
+            <div 
+                className='absolute z-10 border-[black] border-[1px] w-[300px] max-h-[400px] overflow-y-auto bg-white pt-2 pl-1 pr-1'
+                style={{
+                    display: visible ? 'block' : 'none'
+                }}
+                onMouseOutCapture={() => setVisible(false)}
+                onMouseOver={() => setVisible(true)}
+            >
+                {searchKey && isSuccess &&
+                    data &&
+                    data.pages &&
+                    data.pages.map(
+                    (page, pageIndex) =>
+                        page &&
+                        page.result &&
+                        page.result.map((post: IPost, i: number) => {
+                        const newIndex = pageIndex * limit + i
+                        return (
+                            // eslint-disable-next-line no-console
+                            <div key={post._id} className='w-full mb-2' onClick={() => { setVisible(false) }}>
+                            <MinifiedPost
+                                {...post}
+                                index={newIndex + 1}
+                                toggleEditPostModal={toggleEditPostModal}
+                                handleSelectedPost={handleSelectedPost}
+                            />
+                            </div>
+                        )
+                        })
+                )}
+                {
+                    !searchKey  && <div className='w-full h-full flex items-center justify-center pb-2'> No found!</div>
+                }
+                {searchKey && hasNextPage && (
+                    <div className='my-4 w-full z-[1] loader' ref={observerElem}>
+                    <div className='flex items-center justify-center z-[1]'>
+                        <p className='text-white text-sm bg-black px-3 py-2 rounded-lg font-semibold flex flex-row items-center'>
+                        {!isFetchingNextPage ? (
+                            'Load more news...'
+                        ) : (
+                            <React.Fragment>
+                            <span className='animate-spin rotate mr-2'>
+                                <BiLoaderAlt color='white' />
+                            </span>
+                            Loading news...
+                            </React.Fragment>
+                        )}
+                        </p>
+                    </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default SearchModal ;
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 export const ENV = {
   WS_URL: 'wss://localhost:5050',
-  API_URL: 'http://localhost:5050/api'
+  API_URL: 'http://localhost:5050/api',
+
   // WS_URL: 'wss://poc.here.news',
   // API_URL: 'http://poc.here.news/api'
 }
